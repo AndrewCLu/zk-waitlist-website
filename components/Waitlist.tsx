fe396ba8485a1c8aa95ec970313c0487a9274018
AndrewCLu <andrewclu98@gmail.com>
2022-08-25 02:24:18 -0400

    create react component to upload secret for generating commitments

--- components/Waitlist.tsx
+++ components/Waitlist.tsx
@@ -6,20 +6,20 @@ type WaitlistProps = {
 }
 export default function Waitlist (props: WaitlistProps) {
   const { signer } = props;
-  const [accountBalance, setAccountBalance] = useState('');
+  const [displayCommitment, setDisplayCommitment] = useState(false);
+  const [secret, setSecret] = useState('');
+  const [commitment, setCommitment] = useState('');
 
-  useEffect(() => {
-    updateAccountBalance();
-  })
+  const updateSecret = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSecret(e.currentTarget.value);
+  }
+  const resetDisplayCommitment = () => {
+    setDisplayCommitment(false);
+  }
 
-  useEffect(() => {
-    getProof();
-  })
-
-  const updateAccountBalance = async () => {
-    if (!signer) { return; }
-    const balance = await signer.getBalance();
-    setAccountBalance(balance.toString());
+  const generateCommitment = async () => {
+    setCommitment('hey');
+    setDisplayCommitment(true);
   }
 
   const getProof = async () => {
@@ -29,7 +29,27 @@ export default function Waitlist (props: WaitlistProps) {
 
   return (
     <div>
-      Your current account balance is: {accountBalance}
+      { 
+        displayCommitment
+        ? 
+        <div>
+          <div>
+            Here is your commitment:
+            {commitment}
+          </div>
+          <button onClick={resetDisplayCommitment}>Generate new commitment</button>
+        </div>
+        :
+        <div>
+          <form onSubmit={generateCommitment}>
+            <label>
+              Secret:
+              <input type="number" value={secret} onChange={updateSecret} /> 
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      }
     </div>
   )
 }
