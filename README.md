0a39e0a5c0da9a34d75d8e4f90432322394bc655
AndrewCLu <andrewclu98@gmail.com>
2022-08-25 23:45:41 -0400

    refactor commit component

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # ZK Waitlist Frontend
 
 ## TODO
-- See if types can/should be added to api request/response
-- Type/error checking on fetch
-- Validate inputs in form before sending to fetch
-- Send secret with commitment request
-- Figure out how to use commitment from response without causing page refresh
--- components/Waitlist.tsx
+++ components/Waitlist.tsx
@@ -1,64 +1,16 @@
 import { ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
+import Commit from './Commit';
 
 type WaitlistProps = {
   signer?: ethers.Signer;
 }
 export default function Waitlist (props: WaitlistProps) {
   const { signer } = props;
-  const [displayCommitment, setDisplayCommitment] = useState(false);
-  const [secret, setSecret] = useState<string>('');
-  const [commitment, setCommitment] = useState('');
-
-  const updateSecret = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSecret(e.currentTarget.value);
-  }
-  const resetDisplayCommitment = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    setDisplayCommitment(false);
-  }
-
-  const generateCommitment = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (secret.length === 0) { return; }
-    const url = '/api/commitment?secret='+secret;
-    const res = await fetch(url);
-    const json = await res.json();
-    if (res.status === 200) {
-      setCommitment(json.commitment);
-      setDisplayCommitment(true);
-      return;
-    } else {
-      alert('Unable to generate commitment');
-      if (res.status === 400) {
-        console.log(json.error);
-      }
-    }
-  }
 
   return (
     <div>
-      { 
-        displayCommitment
-        ? 
-        <div>
-          <div>
-            Here is your commitment:
-            {commitment}
-          </div>
-          <button onClick={resetDisplayCommitment}>Generate new commitment</button>
-        </div>
-        :
-        <div>
-          <form onSubmit={generateCommitment}>
-            <label>
-              Secret:
-              <input type="number" value={secret} onChange={updateSecret} /> 
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-      }
+      <Commit/>
     </div>
   )
 }
