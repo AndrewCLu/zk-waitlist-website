aba7316a02537619f14cc51ce1cf6dcd95d45f7f
AndrewCLu <andrewclu98@gmail.com>
2022-09-09 02:54:40 -0400

    add github links to home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Metamask, MetamaskConnectionStates } from './Metamask';
-import { Heading } from '@chakra-ui/react';
+import { VStack, Heading, Center, Text, Link } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 type HomeProps = {
   metamaskState: MetamaskConnectionStates;
@@ -8,12 +9,29 @@ type HomeProps = {
 export default function Home(props: HomeProps) {
   return (
     <div>
-      <div>
-        <Heading size="4xl">ZK Waitlist</Heading>
-      </div>
-      <div>
+      <VStack marginTop={'10%'} spacing={'5%'}>
+        <Heading size="4xl" textColor={'purple.500'}>
+          ZK Waitlist
+        </Heading>
+        <Text textColor={'purple.300'} maxWidth={'50%'}>
+          This app demonstrates a reusable on-chain waitlist where users can privately claim and subsequently redeem their spots.
+          <br />
+          <br />
+          Circuits and contracts: <Link href="https://github.com/AndrewCLu/zk-waitlist" isExternal>
+            https://github.com/AndrewCLu/zk-waitlist{' '}
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+          <br />
+          This frontend: <Link
+            href="https://github.com/AndrewCLu/zk-waitlist-website"
+            isExternal
+          >
+            https://github.com/AndrewCLu/zk-waitlist-website{' '}
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </Text>
         <Metamask metamaskState={props.metamaskState} />
-      </div>
+      </VStack>
     </div>
   );
 }
