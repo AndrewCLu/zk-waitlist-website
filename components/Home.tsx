c67060826eb038afece476bf136fcc6a8aa92596
AndrewCLu <andrewclu98@gmail.com>
2022-09-09 03:32:12 -0400

    write text for home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Metamask, MetamaskConnectionStates } from './Metamask';
-import { VStack, Heading, Center, Text, Link } from '@chakra-ui/react';
+import {
+  VStack,
+  Heading,
+  Center,
+  Text,
+  Link,
+  List,
+  OrderedList,
+  ListItem,
+} from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 type HomeProps = {
@@ -13,24 +22,59 @@ export default function Home(props: HomeProps) {
         <Heading size="4xl" textColor={'purple.500'}>
           ZK Waitlist
         </Heading>
-        <Text textColor={'purple.300'} maxWidth={'50%'}>
-          This app demonstrates a reusable on-chain waitlist where users can privately claim and subsequently redeem their spots.
+        <Text textColor={'purple.600'} maxWidth={'60%'}>
+          This app demonstrates a reusable on-chain private waitlist. Through
+          the use of zero knowledge proofs, users can join the waitlist with one
+          account, and later prove from a different account that they had
+          previously joined the waitlist, all without onlookers being able to
+          link the two accounts.
           <br />
           <br />
-          Circuits and contracts: <Link href="https://github.com/AndrewCLu/zk-waitlist" isExternal>
-            https://github.com/AndrewCLu/zk-waitlist{' '}
+          Users interact with the waitlist in 3 phases:
+          <br />
+          <OrderedList>
+            <ListItem>
+              <u>Commit:</u> A user selects a private secret and hashes the
+              secret into a public commitment. The user has joined the waitlist
+              once the commitment is submitted to the waitlist contract.
+            </ListItem>
+            <ListItem>
+              <u>Lock:</u> The waitlist is locked and no one else is allowed to
+              join. The commitments are hashed into a Merkle tree and the root
+              is stored in the contract.
+            </ListItem>
+            <ListItem>
+              <u>Redeem:</u> Any user can submit a proof that they know a secret
+              which corresponds to a valid commitment in Merkle tree. They
+              present this proof along with a second hash of the secret known as
+              the nullifier to the contract, which redeems their spot on the
+              waitlist. The nullifier is used to prevent anyone from reusing the
+              same secret.
+            </ListItem>
+          </OrderedList>
+          <br />
+          Connect to Metamask to get started! Code for the zero knowledge proof
+          circuits, waitlist smart contract, and frontend can be found below.
+        </Text>
+        <Metamask metamaskState={props.metamaskState} />
+        <Text textColor={'purple.800'} maxWidth={'60%'}>
+          Circuits and contracts code:{' '}
+          <Link href="https://github.com/AndrewCLu/zk-waitlist" isExternal>
+            <u>https://github.com/AndrewCLu/zk-waitlist</u>
             <ExternalLinkIcon mx="2px" />
           </Link>
           <br />
-          This frontend: <Link
+          Frontend code:{' '}
+          <Link
             href="https://github.com/AndrewCLu/zk-waitlist-website"
             isExternal
           >
-            https://github.com/AndrewCLu/zk-waitlist-website{' '}
+            <u>https://github.com/AndrewCLu/zk-waitlist-website</u>
             <ExternalLinkIcon mx="2px" />
           </Link>
+          <br />
+          <br />
         </Text>
-        <Metamask metamaskState={props.metamaskState} />
       </VStack>
     </div>
   );
