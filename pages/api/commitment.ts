880fdc1e5974612404b77807a27aea94b8c32cee
AndrewCLu <andrewclu98@gmail.com>
2022-10-01 03:07:52 -0400

    fix circuits file path

--- pages/api/commitment.ts
+++ pages/api/commitment.ts
@@ -3,7 +3,6 @@ import { getErrorMessage } from '../../utils/Errors';
 /* eslint-disable @typescript-eslint/no-var-requires */
 const snarkjs = require('snarkjs');
 const path = require('path');
-const fs = require('fs')
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 // Runs the prover specified by circuit with given input
@@ -13,11 +12,6 @@ const generateCommitmentProof = async (
   input: any
 ): Promise<{ proof: any; publicSignals: any } | Error> => {
   try {
-    const verificationKeyPath = path.resolve(
-      'public/circuits/poseidon_2/poseidon_2_verification_key.json'
-    );
-    const verificationKey = JSON.parse(fs.readFileSync(verificationKeyPath));
-    throw Error(verificationKey.toString())
     const { proof, publicSignals } = await snarkjs.plonk.fullProve(
       input,
       path.resolve('public/circuits/poseidon_2/poseidon_2.wasm'),
